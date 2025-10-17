Added groupby method to generalise combining spectra, fixed aerosol units, fixed summary table generation

eleos/profiles.py
@@ -79,13 +79,13 @@ class Profile:
 
         return new_profile
 
-    def _get_displayable_attributes(self, extra_consts=list(), extra_vars=list()):
+    def _get_displayable_attributes(self, extra_consts=tuple(), extra_vars=tuple()):
         """Get the parameters that define the profile"""
         groups = defaultdict(list)
 
-        for name in self.shape.CONSTANTS + extra_consts:
+        for name in tuple(self.CONSTANTS) + tuple(extra_consts):
             groups[name].append(name)
-        for name in self.shape.VARIABLES + extra_vars:
+        for name in tuple(self.VARIABLES) + tuple(extra_vars):
             groups[name].append(name)
             groups[name].append(name + "_error")
             if self.retrieved:
@@ -133,7 +133,7 @@ class Profile:
                     data[-1].append(f"{color}{pct_diff:+.1f}%{end}")
                 except:
                     data[-1].append(" ")
-        
+
         table = utils.generate_ascii_table(f"{self.label} {self.shape}", headers, [n for n in names], data)
         print(table, **kwargs)
         return table
@@ -220,8 +220,9 @@ class GasProfile(Profile):
         if shape is None:
             raise ValueError("shape attribute must be specified")
         self.shape = shape
-        self.VARIABLES = self.shape.VARIABLES
-        self.CONSTANTS = self.shape.CONSTANTS
+
+        self.VARIABLES = list(self.shape.VARIABLES)
+        self.CONSTANTS = list(self.shape.CONSTANTS)
 
         if not ((gas_id is None) ^ (gas_name is None)):
             raise ValueError("Specifiy exactly one of gas_name or gas_id (not both!)")
@@ -287,6 +288,21 @@ class GasProfile(Profile):
         Returns:
             str: The string to write to the .apr file"""
         return self.create_nemesis_string() + " - " + self.gas_name + "\n" + self.shape.generate_apr_data()
+
+    def sample_from_distribution(self, **kwargs):
+        """
+        Set parameters of the profile shape to random values pulled from a normal distribution, with arguments mean_[parameter] and std_[parameter].
+        For example: calling GasProfile.sample_from_distribution(mean_deep_vmr=0.1, std_deep_vmr=0.05) will set the deep vmr to a random value drawn 
+        from a gaussian with the centre at 0.1 and a standard deviation of 0.05. Multiple parameters may be specified at once.
+        
+        Args:
+            * mean_[parameter]: The mean of the distribution to sample for parameter [parameter]
+            * std_[parameter]:  The standard deviation of the distribution to sample for parameter [parameter]
+        
+        Returns:
+            None
+        """
+        self.shape.sample_from_distribution(**kwargs)
 
 
 class AerosolProfile(Profile):
@@ -450,9 +466,6 @@ class AerosolProfile(Profile):
         prof.aerosol_id = prev.aerosol_id
         return prof
 
-    def _get_displayable_attributes(self):
-        return super()._get_displayable_attributes(extra_vars=["radius", "variance", "imag_n"])
-
     def create_nemesis_string(self):
         """Create the NEMESIS code that represents the aerosol profile (eg. -1 0 32)
         
@@ -507,6 +520,33 @@ class AerosolProfile(Profile):
             utils.write_nums(file, refwave)
             for imag_n, wavelength in zip(imag_ns, xsc_parser.xsc.wavelength):
                 utils.write_nums(file, wavelength, imag_n, self.imag_n_error)
+
+    def sample_from_distribution(self, **kwargs):
+        """
+        Set parameters of the profile and profile shape to random values pulled from a normal distribution, with arguments mean_[parameter] and std_[parameter].
+        For example: calling AerosolProfile.sample_from_distribution(mean_radius=0.1, std_radius=0.05) will set the particle radius to a random value drawn 
+        from a gaussian with the centre at 0.1 and a standard deviation of 0.05. Multiple parameters may be specified at once. If only
+        the standard deviation is given for a parameter then use the current value of that parameter as the mean - ie add a random perturbation to the value)
+        
+        Args:
+            * mean_[parameter]: The mean of the distribution to sample for parameter [parameter]
+            * std_[parameter]:  The standard deviation of the distribution to sample for parameter [parameter]
+        
+        Returns:
+            None
+        """
+        # set profile-level parameters (radius, variance, real_n, imag_n)
+        for param in kwargs.keys():
+            for vp in ["radius", "variance", "real_n", "imag_n"]:
+                if vp in param:
+                    if f"mean_{vp}" in kwargs.keys():
+                        mean = kwargs[f"mean_vp"]
+                    else:
+                        mean = getattr(self, vp)
+                    setattr(self, vp, np.random.normal(mean, kwargs[f"std_{vp}"]))
+        
+        # and pass the rest to the Shape object
+        self.shape.sample_from_distribution(**kwargs)
 
 
 def get_parameter_names(base, retrieved=True):

eleos/results.py
@@ -7,6 +7,7 @@ import itertools
 import numpy as np
 from functools import wraps
 from pathlib import Path
+from astropy import units as u
 
 import warnings
 warnings.formatwarning = lambda msg, *_: f"Warning: {msg}\n"
@@ -152,6 +153,61 @@ class NemesisResult:
 
             return values
 
+    def _convert_aerosol_units(self, data, p_ref=1):
+        """
+        Convert the aerosol densities in the aerosol.prf file to more useful units. This assumes that the cross-sections are normalised at a specific wavelength, such that the native units are cm2/g.
+        This function converts them to optical depth per km, optical depth per bar, and pressure-integrated optical depth.
+
+        Args:
+            data (np.array): Aerosol densities in cm2/g
+            p_ref (float):   The reference pressure in bar at which to calculate everything. Default is 1 bar and should not need to be changed.
+
+        Returns:
+            opacity_ODkm (np.array):  Aerosol densities in optical depth per km
+            opacity_ODbar (np.array): Aerosol densities in optical depth per bar
+            opacity_ODkm_int (np.array): Aerosol densities in optical depth integrated over height
+            opacity_ODbar_int (np.array): Aerosol densities in optical depth integrated over pressure
+
+        """
+
+        # first, calculate the average molecular weight of the atmosphere
+        masses = []
+        for gas in self.core.ref.data.columns:
+            if gas in ("height", "pressure", "temperature"):
+                continue
+            g = gas.split(" ")[0] # ignore isotope weights
+            m = constants.GASES.loc[constants.GASES["name"] == g, "molar_mass"].iloc[0]
+            masses.append(m)
+        vmrs = self.core.ref.data.copy()
+        x = vmrs.drop(columns=["height", "pressure", "temperature"])
+        vmrs["mean_molar_wt"] = (x * masses).sum(axis=1)
+
+        # then calculate the values at the target level
+        target = vmrs.loc[utils.find_nearest(self.core.ref.data["pressure"], p_ref)[0]]
+        pres = target["pressure"] * u.bar
+        temp = target["temperature"] * u.K
+        mol_wt = target["mean_molar_wt"] * u.g / u.mol
+        gravity = constants.GRAVITY[self.core.planet] * u.m / u.s**2
+        R = 8.314 * u.J / u.K / u.mol
+
+        Hscale = ((R * temp) / (mol_wt * gravity)) 
+        # print("Atmosphere scale height: ", Hscale.decompose())
+
+        rho = ((mol_wt * pres) / (R * temp))
+        # print("Atmospheric density: ", rho.decompose())
+
+        opacity_ODkm = list(data) * u.cm**2/u.g * rho
+        opacity_ODkm = opacity_ODkm.to(1 / u.km)
+        # print("Maximum optical depth per km: ", max(opacity_ODkm))
+
+        opacity_ODbar = opacity_ODkm * Hscale / pres
+        opacity_ODbar = opacity_ODbar.to(1 / u.bar)
+        # print("Maximum optical depth per bar: ", max(opacity_ODbar))
+
+        opacity_ODbar_int = np.cumsum(opacity_ODbar)
+
+        return opacity_ODkm.value, opacity_ODbar.value, opacity_ODbar_int.value
+
     def print_summary(self, colors=False):
         print(f"Summary of retrieval in {self.core_directory}")
         print()
@@ -277,8 +333,6 @@ class NemesisResult:
             matplotlib.Figure: The Figure object to which the Axes belong
             matplotlib.Axes: The Axes object onto which the data was plotted"""
 
-        print(self.core.ref)
-
         # Get the appropriate y axis data
         y = self.core.ref.pressure if pressure else self.core.ref.height
         
@@ -305,7 +359,11 @@ class NemesisResult:
         
         Args:
             ax: The matplotlib.Axes object to plot to. If omitted then create a new Figure and Axes
-            unit: The unit to convert the aerosol profiles to. Valid values are 'tau/bar' for optical thickness/bar, 'cm2/g' for the native prf units
+            unit: The unit to convert the aerosol profiles to. Valid values are:
+                  'tau/bar' for optical depth per bar, 
+                  'tau/km' for optical depth per bar, 
+                  'tau' for pressure-integrated optical depth, 
+                  'cm2/g' for the native prf units
             pressure: Whether to plot the aerosol profiles against pressure (if True) or height (if False)
             
         Returns:
@@ -325,24 +383,31 @@ class NemesisResult:
             x = self.retrieved_aerosols[label]
             y = self.retrieved_aerosols.pressure if pressure else self.retrieved_aerosols.height
 
+            tau_per_km, tau_per_bar, tau_integrated_bar = self._convert_aerosol_units(x)
+
             if unit == "tau/bar":
-                # only god himself knows whats going on with these units...
-                x *= 1e5 / 10 / constants.GRAVITY[self.core.planet]
-                unit_label = f"Optical depth / bar at {self.core.reference_wavelength:.1f}µm"
-            elif unit == "particles/g":
-                unit_label = f"Aerosol specific density (particles / gram) at {self.core.reference_wavelength:.1f}µm)"
+                unit_label = f"Optical depth / bar"
+                x = tau_per_bar
+            elif unit == "tau/km":
+                unit_label = f"Optical depth / km"
+                x = tau_per_km
+            elif unit == "tau":
+                unit_label = f"Pressure-integrated optical depth"
+                x = tau_integrated_bar
+            elif unit == "cm2/g":
+                unit_label = f"Aerosol density (cm$^2$ / gram)"
             else:
-                raise ValueError(f"Invalid unit! - Must be one of 'tau/bar', 'particles/g' - not {unit}")
+                raise ValueError(f"Invalid unit! - Must be one of 'tau/bar' 'tau/km' 'tau' or 'cm2/g' - not {unit}")
+            unit_label += f" at {self.core.reference_wavelength:.1f}µm"
             
             if x.max() > max_value:
                 max_value = x.max()
 
-            # ax.fill_betweenx(y, 1e-12, x, label=label, facecolor="#A21700")
             ax.plot(x, y, label=label)
 
         ax.set_xlabel(unit_label)
         ax.set_xscale("log")
-        ax.set_xlim(1e-6, max_value*2)
+        ax.set_xlim(1e-4, 1e2)
         ax.legend()
 
     @plotting_altitude
@@ -382,6 +447,7 @@ class NemesisResult:
 
         # Get the prior distributions if requested
         if plot_prior_profiles:
+            warnings.warn("Generating the prior distributions may be broken! Check outputs carefully!")
             priors = self.core.generate_prior_distributions()
             y3 = priors["pressure"] if pressure else priors["height"]
 

eleos/shapes.py
@@ -40,6 +40,8 @@ Class template::
 
 """
 
+import numpy as np
+
 from dataclasses import dataclass
 from typing import ClassVar
 import shutil
@@ -85,6 +87,30 @@ class Shape:
     def create_required_files(self, directory):
         """Some Shapes require additional files to be created/copied into the core directory"""
         pass
+
+    def sample_from_distribution(self, **kwargs):
+        """
+        Set parameters of the Shape to random values pulled from a normal distribution, with arguments mean_[parameter] and std_[parameter].
+        For example: calling Shape4.sample_from_distribution(mean_deep_vmr=0.1, std_deep_vmr=0.05) will set the deep vmr to a random value drawn 
+        from a gaussian with the centre at 0.1 and a standard deviation of 0.05. Multiple parameters may be specified at once.
+        
+        Args:
+            * mean_[parameter]: The mean of the distribution to sample for parameter [parameter]
+            * std_[parameter]:  The standard deviation of the distribution to sample for parameter [parameter]
+        
+        Returns:
+            None
+        """
+
+        for param in kwargs.keys():
+            for vp in self.CONSTANTS + self.VARIABLES:
+                if vp in param:
+                    if f"mean_{vp}" in kwargs.keys():
+                        mean = kwargs[f"mean_vp"]
+                    else:
+                        mean = getattr(self, vp)
+                    setattr(self, vp, np.random.normal(mean, kwargs[f"std_{vp}"]))
+        
 
 
 @shapeclass
@@ -133,7 +159,6 @@ class Shape0(Shape):
 
     def generate_apr_data(self):
         return self.filepath.name
-
 
 @shapeclass
 class Shape1(Shape):

eleos/spectra.py
@@ -4,6 +4,7 @@ import glob
 import h3ppy
 import planetmapper
 import copy
+import warnings
 from astropy.io import fits
 from collections import defaultdict
 from scipy.optimize import curve_fit
@@ -14,7 +15,6 @@ from scipy.signal import fftconvolve
 from . import utils
 from . import spx
 from . import constants
-
 
 def margin_trim(cube, margin_size=3):
     """
@@ -335,7 +335,7 @@ def zonal_average_tiles(filepaths, lat_width, error_scale=1, rmsd_threshold=10, 
     Returns:
         np.ndarray:       Wavelength grid, temporary - will be added to df later
         pandas.DataFrame: DataFrame containing the zonal means and information for .spx files.
-                          Columns are spectrum, error (np.ndarray's), phase, emission, azimuth, lon (floats), num_spaxels (int).
+                          Columns are spectrum, error (np.ndarrays), phase, emission, azimuth, lon (floats), num_spaxels (int).
                           Use df.index to get latitudes"""
     
     if filters is None:
@@ -423,6 +423,147 @@ def zonal_average_tiles(filepaths, lat_width, error_scale=1, rmsd_threshold=10, 
     del zonal["nans"], zonal["rmsd"]
 
     return obs.get_wavelengths_from_header(), zonal
+
+
+def groupby(filepaths, by="EMISSION", binsize=2, binstart=0, binend=90, rmsd_threshold=10, filters=None):
+    """Group spaxels from a set of JWST navigated cubes by a given backplane value.
+    
+    Args:
+        filepaths (List(str)): List of filepaths to tiles to use
+        by (str):              The planetmapper backplane name to group by
+        binsize (float):       The size of the bins to group by
+        binstart (float):      The start of the first bin (if None then use min of backplane)
+        binend (float):        The end of the last bin (if None then use max of backplane)
+        rmsd_threshold (int):  Reject any spaxels that are in the top x percentile for root-mean-square deviation from the median 
+        filters (dict):        Apply any filters to remove spaxels before averaging. Format is key=planetmapper backplane name (str)
+                               and value=(min, max). For example, filters={'EMISSION':(0, 70)} will accept only spaxels with emission angles between 0 and 70
+    
+    Returns:
+        np.ndarray:       Wavelength grid, temporary - will be added to df later
+        pandas.DataFrame: DataFrame containing the means and information for .spx files.
+                          Columns are spectrum, error (np.ndarrays), phase, emission, azimuth, lon (floats), num_spaxels (int).
+                          Use df.index to get the bin centres
+    """
+    if filters is None:
+        filters = dict()
+
+    # Convert the list of tiles into a single dataframe of spaxels
+    spaxels = flatten_tiles(filepaths)
+    spaxels["spectrum"] = spaxels["spectrum"].apply(lambda x: np.where(np.array(x) < 0, np.nan, x))
+
+    # Check valid backplane name
+    if by not in spaxels.columns:
+        raise ValueError(f"Backplane {by} not found in spaxels. Available backplanes are: {list(spaxels.columns)}")
+
+    # Set default bin edges
+    if binstart is None:
+        binstart = np.nanmin(spaxels[by])
+    if binend is None:
+        binend = np.nanmax(spaxels[by])
+    
+    # Apply any filters to remove spaxels before averaging
+    for filter_bp, bounds in filters.items():
+        if filter_bp not in spaxels.columns:
+            raise ValueError(f"Backplane {filter_bp} not found in spaxels. Available backplanes are: {list(spaxels.columns)}")
+        spaxels = spaxels[(spaxels[filter_bp] > bounds[0]) & (spaxels[filter_bp] < bounds[1])]
+
+    # Bin the spaxels by the given backplane
+    cut = pd.cut(spaxels[by], np.arange(binstart, binend, binsize))
+    grouped = spaxels.groupby(cut)   
+    nonzero = 0
+    for name, df in grouped:
+        nonzero += len(df) > 0 
+
+    i = 0
+    c = [plt.get_cmap("viridis")(j) for j in np.linspace(0, 1, nonzero)]
+    for name, df in grouped:
+        if len(df) == 0:
+            continue
+
+        # Add column to allow grouping by wavelength setting
+        df["_wl_key"] = df["wavelength"].apply(lambda x: x[0])
+        filtergrouped = df.groupby("_wl_key")
+
+        # Lists to store the individual spectra to combine multiple gratings
+        wls = []
+        spec = []
+        errs = []
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            
+            # Iterate over each unique wavelength grid (ie. each filter/grating combination)
+            for wl, ddf in filtergrouped:
+                print(f"Processing {name} group starting at {wl:2f} um with {len(ddf)} spaxels")
+
+                # Calculate group median
+                spectra = np.stack(ddf["spectrum"].values)
+                median = np.nanmedian(spectra, axis=0)
+
+                # Check for all-nan spectrum and skip if so
+                if np.all(np.isnan(median)):
+                    print("Skipping all-nan bin")
+                    continue
+
+                # Caclulate rmsd and remove outliers
+                ddf["rmsd"] = np.sqrt(np.nanmean((spectra - median)**2, axis=1))
+                filtered_ddf = ddf.drop(ddf[(ddf.rmsd > np.percentile(ddf.rmsd, 100-rmsd_threshold))].index)
+                print(f"Rejected {len(ddf) - len(filtered_ddf)} outliers out of {len(ddf)} spaxels")
+                    
+                # Add to list of spectra to combine multiple gratings
+                wls.append(ddf.iloc[0].wavelength)
+                spec.append(np.nanmean(np.stack(filtered_ddf["spectrum"].values), axis=0))
+                errs.append(np.nanmean(np.stack(filtered_ddf["error"].values), axis=0))
+
+
+        if len(spec) != 0:
+            w,s,e = combine_multiple_spectra(*zip(wls, spec, errs))
+            plt.plot(w, s, 
+                        c=c[i],
+                        label=f"{name}")
+            i += 1
+
+    plt.legend()
+    plt.yscale("log")
+    plt.show()
+
+
+def flatten_tiles(filepaths):
+    """Flatten a set of navigated cubes into a single dataframe of spaxels"""
+
+    # Dataframe to hold individual spaxels, final zonal spectra, various angles
+    spaxels = pd.DataFrame()
+
+    # Iterate over each tile in the mosaic
+    for tile in filepaths:
+        print("[eleos] Processing ", tile)
+
+        pd.set_option('display.max_columns', 6)
+        pd.set_option('display.width', 120)
+
+        # Get planetmapper Observation object
+        obs = planetmapper.Observation(tile)
+        obs = add_error_cubes(obs)
+
+        # Rearrange spaxels into list such that:
+        # obs.data[:,a,b] --> pixels[b + obs.data.shape[2]*a]
+
+        df = pd.DataFrame()
+    
+        wavelength = obs.get_wavelengths_from_header()
+        spectra = np.moveaxis(obs.data, 0, -1).reshape(-1, obs.data.shape[0])
+        errors = np.moveaxis(obs.error, 0, -1).reshape(-1, obs.data.shape[0])
+
+        df["wavelength"] = [wavelength] * spectra.shape[0]
+        df["spectrum"] = spectra.tolist()
+        df["error"] = errors.tolist()
+
+        for backplane in obs.backplanes.keys():
+            df[backplane] = obs.get_backplane_img(backplane).flatten()
+
+        spaxels = pd.concat([spaxels, df], ignore_index=True)
+
+    return spaxels
 
 
 def multiple_cube_average(cubes):